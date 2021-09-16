4ad5313f0db49179baac113f4b51ad3898be4769
Kirill Alekseev <60962145+alllirik@users.noreply.github.com>
2021-09-16 13:19:30 +0300

    Update sfs.sh

--- sfs.sh
+++ sfs.sh
@@ -1,17 +1,24 @@
-ANGSD =
-SAMtools =
-ANC=Homo_sapiens_assembly38.fasta
-REF=Homo_sapiens_assembly38.fasta
-BAM_PATH =
-COV=2X
-FRAC=06
+ANC=human_g1k_v37.fasta
+REF=human_g1k_v37.fasta
+BAM_PATH=bams
+MEAN_COV=8
+COV_ARRAY=(4 2)
+MODEL_ARRAY=(1 2)
 
-for BAM in BAM_PATH/*.bam
+for COV in ${COV_ARRAY[*]}
 do
-	$SAMtools view -bs 42.$FRAC "${BAM##*/}" > $COV."${BAM##*/}"
+	FRAC=($(bc <<< "scale=2;$COV/$MEAN_COV"))
+	mkdir ${COV}X
+	for BAM in $BAM_PATH/*.bam
+	do
+		samtools view -bs 42$FRAC $BAM > ${COV}X/${COV}X."${BAM##*/}"
+	done
+	ls ${COV}X/*.bam > ${COV}X.filelist
+	for MODEL in ${MODEL_ARRAY[*]}
+	do
+		angsd -b ${COV}X.filelist -anc $ANC -ref $REF -doCounts 1 -baq 1 -C 50 -minMapQ 30 -minQ 20 -minInd 2 -P 2 -GL $MODEL -doSaf 1 -out ${COV}X/GL${MODEL}.${COV}X 
+		realSFS -maxIter 100 -P 4 ${COV}X/GL${MODEL}.${COV}X.saf.idx > ${COV}X/GL${MODEL}.${COV}X.sfs
+	done
 done
-
-ls $COV* > $COV.filelist
-$ANGSD/angsd -b $COV.filelist -anc $ANC -ref $REF -doCounts 1 -baq 1 -C 50 -minMapQ 30 -minQ 20 -minInd 2 -P 2 -GL 2 -doSaf 1 -fold 1 -out Results/$COV
-$ANGSD/misc/realSFS -maxIter 100 -P 2 Results/$COV.saf.idx > Results/$COV.sfs
+Rscript sfs_barplot.R
 
