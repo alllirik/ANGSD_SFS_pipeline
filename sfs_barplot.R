411c2c1072622e9cdbdf00d0f0598ac47e105587
Kirill Alekseev <60962145+alllirik@users.noreply.github.com>
2021-09-16 13:19:59 +0300

    Update sfs_barplot.R

--- sfs_barplot.R
+++ sfs_barplot.R
@@ -1,16 +1,27 @@
-nnorm <- function(x) x/sum(x)
-res <- rbind(
-  GL1=scan("path/to/file/filename.GL1.2X.sfs")[-1],
-  GL2=scan("path/to/file/filename.GL2.2X.sfs")[-1],
-  GL3=scan("path/to/file/filename.GL3.2X.sfs")[-1]
-)
-
-#density instead of expected counts
-res <- t(apply(res,1,nnorm))
-
-#plot the none ancestral sites
-barplot(res,beside=T,main="Folded SFS 2X", names=1:4, col=c("lightblue","blue","darkblue"))
-legend("topright", 
-       fill = c("lightblue","blue","darkblue"),
-       legend = c("SAMtools", "GATK", "SYK"), 
-       cex = 0.35)
+for(i in seq(2, 4, by=2))
+{
+  print(i)
+  nnorm <- function(x) x/sum(x)
+  path_1 = paste(c(i,"X/GL1.",i,"X.sfs"), collapse = "")
+  path_2 = paste(c(i,"X/GL2.",i,"X.sfs"), collapse = "")
+  res <- rbind(
+    GL1=scan(path_1)[-1],
+    GL2=scan(path_2)[-1]
+  )
+  #density instead of expected counts
+  res <- t(apply(res,1,nnorm))
+  
+  name=paste(c("SFS_",i,"X.pdf"), collapse = "")
+  pdf(file = name,   # The directory you want to save the file in
+      width = 6, # The width of the plot in inches
+      height = 4) # The height of the plot in inches
+  
+  #plot the none ancestral sites
+  barplot(res,beside=T,main="Folded SFS", names=1:20, col=c("lightblue","blue"))
+  legend("topright", 
+         fill = c("lightblue","blue"),
+         legend = c("SAMtools", "GATK"), 
+         cex = 0.35)
+  
+  dev.off()
+}
